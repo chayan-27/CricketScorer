47a414da47c2e08c97ca818acae8d733864ecc85
chayan-27 <63251971+chayan-27@users.noreply.github.com>
2020-10-09 21:01:46 +0530

    Animators Added

--- app/src/main/java/com/chayan_27/cricketscorer/MainActivity.java
+++ app/src/main/java/com/chayan_27/cricketscorer/MainActivity.java
@@ -2,10 +2,17 @@ package com.chayan_27.cricketscorer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.BounceInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.plattysoft.leonids.ParticleSystem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +31,85 @@ public class MainActivity extends AppCompatActivity {
         runs = (EditText) findViewById(R.id.runs);
         balls = (EditText) findViewById(R.id.balls);
         cal = (Button) findViewById(R.id.cal);
+        final ImageView imageView = (ImageView) findViewById(R.id.image1);
+        ImageView imageView1=(ImageView) findViewById(R.id.image2);
+
+        final float bottomOfScreen = getResources().getDisplayMetrics()
+                .heightPixels - (imageView.getHeight() * 4);
+        //bottomOfScreen is where you want to animate to
+        animator(imageView,"translationX",-1000f,1000f,5000);
+        animator(imageView,"translationY",bottomOfScreen,0f,5000);
+        animator(imageView1,"translationX",1000f,-1000f,5000);
+        animator(imageView,"translationY",1000f,-1000f,5000);
+        /*ObjectAnimator animY = ObjectAnimator.ofFloat(imageView, "rotation", -1000f,1000f);
+        animY.setDuration(5000);
+        animY.setInterpolator(new AccelerateInterpolator());
+
+        animY.setRepeatCount(200);
+        animY.start();
+        ObjectAnimator animY1 = ObjectAnimator.ofFloat(imageView, "translationY", bottomOfScreen,0f);
+        animY1.setDuration(5000);
+        animY1.setInterpolator(new AccelerateInterpolator());
+
+        animY1.setRepeatCount(200);
+        animY1.start();
+
+        ObjectAnimator animX = ObjectAnimator.ofFloat(imageView1, "translationX", 1000f,-1000f);
+        animX.setDuration(3000);
+        animX.setInterpolator(new AccelerateInterpolator());
+
+        animX.setRepeatCount(200);
+        animX.start();
+        ObjectAnimator animX1 = ObjectAnimator.ofFloat(imageView1, "rotation", 1000f,-1000f);
+        animX1.setDuration(3000);
+        animX1.setInterpolator(new AccelerateInterpolator());
+
+        animX1.setRepeatCount(200);
+        animX1.start();*/
+
+      /*  imageView.animate()
+                .translationY(bottomOfScreen)
+                .rotation(90)
+                .setInterpolator(new AccelerateInterpolator())
+                .setInterpolator(new BounceInterpolator())
+                .setDuration(2000)
+        .setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animation.start();
+
+
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        })
+        ;*/
+        /*new ParticleSystem(this, 80, R.drawable.ic_cricket_02, 10000)
+                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 180)
+                .setRotationSpeed(144)
+
+                .setAcceleration(0.00005f, 90)
+                .emit(findViewById(R.id.image1), 8);
+
+        new ParticleSystem(this, 80, R.drawable.ic_cricketball, 10000)
+                .setSpeedModuleAndAngleRange(0f, 0.3f, 0, 0)
+                .setRotationSpeed(144)
+                .setAcceleration(0.00005f, 90)
+                .emit(findViewById(R.id.image2), 8);*/
     }
 
     public void calculate(View view) {
@@ -108,5 +194,13 @@ public class MainActivity extends AppCompatActivity {
                 list.add(result);
             }*/
         }
+    }
+
+    public void animator(ImageView imageView,String propertyname,float left,float right,long duration){
+        ObjectAnimator animY = ObjectAnimator.ofFloat(imageView, propertyname,left,right);
+        animY.setDuration(duration);
+        animY.setInterpolator(new AccelerateInterpolator());
+        animY.setRepeatCount(200);
+        animY.start();
     }
 }
